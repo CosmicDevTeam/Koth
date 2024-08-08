967d7d3ff84b3d5bf9adbb7e893490e2043d9f9f
Zephyrhz0 <162658447+CosmicDevTeam@users.noreply.github.com>
2024-08-08 16:12:22 -0700

    Create EffectsFilter.php

---
+++ src/zephy/koth/events/EffectsFilter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace zephy\koth\events;
+
+
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\StringToEffectParser;
+use pocketmine\player\Player;
+
+class EffectsFilter {
+   public static function applyEffect(Player $player, string $name, int $duration = 50, int $level = 1): bool{
+      $effect = StringToEffectParser::getInstance()->parse($name);
+      $player->sendMessage($name);
+                           
+		if($effect === null){
+		   return false;
+		} 
+		   
+		$time = $duration * 20;
+		
+		if($player->getEffects()->has($effect)){
+		   return false;
+		}
+		
+		$add = new EffectInstance($effect, $time, $level);
+		$player->getEffects()->add($add);
+        return true;
+   }
+}
