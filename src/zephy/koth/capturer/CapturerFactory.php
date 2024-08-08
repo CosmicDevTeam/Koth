b7e59473923ca0d606e534bdb249d5f4a4936ca4
Zephyrhz0 <162658447+CosmicDevTeam@users.noreply.github.com>
2024-08-08 16:22:18 -0700

    Create CapturerFactory.php

---
+++ src/zephy/koth/capturer/CapturerFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace zephy\koth\capturer;
+
+use zephy\koth\data\Koth;
+use pocketmine\utils\SingletonTrait;
+class CapturerFactory {
+   use SingletonTrait;
+   
+   private array $capturers = [];
+   
+   public function addCapturer(string $player, Koth $koth): void {
+      $this->capturers[$player] = new Capturer($koth, $player);
+   }
+   public function getCapturer(string $player): ?Capturer {
+      return $this->capturers[$player] ?? null;
+   }
+   public function deleteCapturer(string $player): void {
+      unset($this->capturers[$player]);
+   }
+   public function unsetAll(): void {
+       unset($this->capturers);
+   }
+   public function getCapturers(): array {
+      $capturers = [];
+      
+      foreach($this->capturers as $player => $capturer){
+         $capturers[$player] = $capturer->getSeconds();
+      }
+      return $capturers;
+   }
+   
+   
+}
